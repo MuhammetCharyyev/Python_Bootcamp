9ebd137176a71561f73e2c5bf06a6fd6226f19c4
Muhammet <muhammedabuchary@gmail.com>
2023-10-03 23:05:23 -0500

    custom_class

--- day2/custom_class.py
+++ day2/custom_class.py
@@ -2,7 +2,8 @@ import numbers
 
 
 class Employee:
-    is_human = True  # similar to static variable of Java
+    is_human = True  # similar to static variable of Java, as there are no static variables in PY
+    # the static variable should outside the constructor
     planet = 'Earth'
 
     def __init__(self, name: str = 'Unknown', job_title: str = 'Janitor', salary: numbers = 0):
@@ -12,8 +13,8 @@ class Employee:
         self.job_title = job_title
         self.salary = salary
 
-    def work(self):
-        print(f'{self.name} is working')
+    def work(self): # instance method
+        print(f'{self.name} is working') # f - is for concatenation purpose
 
     def __str__(self):
         return f'{type(self).__name__}{self.__dict__}'
@@ -43,7 +44,7 @@ print(employee4.name)
 print(employee4.job_title)
 print(employee4.salary)
 
-# print(Employee.name)
+# print(Employee.name) # as it is not static variables we are not able to revoke it by the class name
 print(Employee.is_human)
 print(Employee.planet)
 
